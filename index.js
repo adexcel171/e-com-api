16c755fbe2f8568375ae735ce9daf9c6b6d9009b
admire Excel <godwillokenyi@gmail.com>
2023-03-13 15:05:19 +0100

    api

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+
+mongoose.connect( "mongodb+srv://godswill:unlimited1@cluster0.mjfq3nb.mongodb.net/shop?retryWrites=true&w=majority").then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use (express.json());
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(5000, () => {
+  console.log("Backend server is running!");
+});

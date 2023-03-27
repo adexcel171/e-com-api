8f122a189989af1c260280b133d0369d9e8fc626
admire Excel <godwillokenyi@gmail.com>
2023-03-27 16:43:35 +0100

    index

--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
         isAdmin: user.isAdmin,
       },
       process.env.JWT_SEC,
-      {expiresIn:"2d"}
+      {expiresIn:"4d"}
     );
 
     const { password, ...others } = user._doc;

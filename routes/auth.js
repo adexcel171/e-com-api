44e4ac26deeaba8df9311aba3ed9f2175b4adcb3
admire Excel <godwillokenyi@gmail.com>
2023-03-27 18:19:47 +0100

    add

--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
         isAdmin: user.isAdmin,
       },
       process.env.JWT_SEC,
-      {expiresIn:"4d"}
+      {expiresIn:"3d"}
     );
 
     const { password, ...others } = user._doc;
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const User = require("../models/User");
-const router = require("express").Router();
 const {
   verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
+const router = require("express").Router();
 
 //UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {

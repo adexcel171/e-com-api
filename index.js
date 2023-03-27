ec6f07acc582d6c3863350d06020afad88221cef
admire Excel <godwillokenyi@gmail.com>
2023-03-27 18:19:45 +0100

    add

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 
-mongoose.connect(process.env.MONGO_URL, () => console.log("DB Connection Successfull!"))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
 
-app.use (express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -26,6 +28,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT,  () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });

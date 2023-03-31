f1b6acd2741182e63739ef9f51c0f595e8dc02f0
admire Excel <godwillokenyi@gmail.com>
2023-03-31 10:21:25 +0100

    mongo connection

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
+mongoose.connect(process.env.MONGO_URL, () => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
 
+app.use (express.json());
 app.use(cors());
-app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -28,6 +26,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT,  () => {
   console.log("Backend server is running!");
 });

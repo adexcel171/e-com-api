768aac695f1c5a92470d82a517bd0cad37444f32
admire Excel <godwillokenyi@gmail.com>
2023-08-09 21:45:20 +0100

    ready

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ mongoose.connect(process.env.MONGO_URL, () => console.log("DB Connection Success
     console.log(err);
   });
 
-app.use (express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -26,6 +26,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT,  () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
